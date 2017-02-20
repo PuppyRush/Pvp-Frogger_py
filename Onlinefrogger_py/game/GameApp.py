48051200b8476e7e3638ae70884a46a1700cbec5
PuppyRush <gooddaumi@naver.com>
2017-02-21 01:44:46 +0900

    resolve confliction

--- Onlinefrogger_py/game/GameApp.py
+++ Onlinefrogger_py/game/GameApp.py
@@ -42,15 +42,9 @@ SURFACE.fill(BACKGROUND_COLOR)
 
 class GameApp(object):
 
-<<<<<<< HEAD
-    def __init__(self,players=[],level=int,speed=int):
-
-        self.__GAP_OF_FROG = GameInfo.SCREEN_WIDTH_SIZE.value /(len(players)+1)
-=======
     def __init__(self,ctl=Controller.Controller, players=[],level=int,speed=int,maxHeightCount=int):
         self.ctl = ctl
         self.__GAP_OF_FROG = GameInfo.WIDTH_COUNT.value * GameInfo.WIDTH_SIZE.value /(len(players)+1)
->>>>>>> refs/remotes/origin/whydisable
         self.__level = level
         self.__speed = speed
         self.MAX_HEIGHT_COUNT = maxHeightCount
@@ -65,14 +59,9 @@ class GameApp(object):
         for i in players:
             x = int((order+1)*GameInfo.WIDTH_COUNT.value/(len(players)+1))
             y = 0
-<<<<<<< HEAD
-            self.players.append( game.Player.Player(i[0], i[1], [ (order+1)*self.__GAP_OF_FROG - GameInfo.WIDTH_SIZE.value/2-3, GameInfo.HEIGHT_COUNT.value] , 
-                                                    [ x,y ] ))
-=======
             self.players.append( game.Player.Player(self.ctl,i[0], i[1], 
                                                     [ (order+1)*self.__GAP_OF_FROG, GameInfo.HEIGHT_COUNT.value], 
                                                     [ x,y ]) )
->>>>>>> refs/remotes/origin/whydisable
             order+=1
           
     def __setMap(self):
@@ -92,30 +81,7 @@ def beginGameApp(playeres=[],level=int,speed=int):
     pygame.init()
     pygame.display.set_caption("FROGGER")
     
-<<<<<<< HEAD
-    
-    screenBeginIdx=0
-    screenEndIdx=29
 
-    startSection = True
-    EndSection = False
-
-    idxGap=0
-
-    while True:
-
-        for player in gameApp.players:
-            key = player.updateKeyEvent(pygame.key.get_pressed())
-            if(key == Player.Order.UP):
-                if(startSection==False and EndSection==False):
-                    idxGap+=1
-
-            if(player.idx[1] == GameInfo.SCREEN_HEIGHT_SIZE.value/2):
-                startSection = False
-                EndSection = False
-                     
-        for i in range(screenBeginIdx ,screenEndIdx+1):
-=======
     maxHeightCount = Map.Map.getMaxHeightCount(GameInfo.HEIGHT_COUNT.value,level)
 
     ctl = Controller.Controller(maxHeightCount)
@@ -131,7 +97,6 @@ def beginGameApp(playeres=[],level=int,speed=int):
             player.updateKeyEvent(pygame.key.get_pressed())
 
         for i in range(ctl.getLowerScreenIdx(), ctl.getUpperScreenIdx()):
->>>>>>> refs/remotes/origin/whydisable
 
             for l in range(0,GameInfo.WIDTH_COUNT.value):
                 if(gameApp.map.earth[i].mapKind == Map.MapEnum.ROCK):
@@ -152,17 +117,6 @@ def beginGameApp(playeres=[],level=int,speed=int):
                 SURFACE.blit( l.image, l.getPosition(i))
                 l.update(DELTAT)
 
-<<<<<<< HEAD
-            #judgetment
-
-                        
-                
-
-            #gameApp.players[0].decisionFrog(gameApp.hecklers[i],gameApp.map.earth[i])
-
-
-=======
->>>>>>> refs/remotes/origin/whydisable
         pygame.display.flip()
         pygame.display.update()
 
